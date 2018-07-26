7ff927a64f493845a6fa7f10e0360c9400a10bd6
zhanghongqing@interrcs.com <zhanghongqing@interrcs.com>
2018-07-26 18:27:59 +0800

    * init

---
+++ app/centre/main/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Layout, Menu, Button} from 'antd';
+import {HashRouter as Router,Link, Switch, Route} from 'react-router-dom';
+import NotFound from '../../notfound';
+import './main.less';
+const {SubMenu} = Menu;
+const {Header, Content, Sider} = Layout;
+
+export default class Centre extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentWillUnmount() {
+
+    }
+
+    render=()=> {
+        return (
+            <Layout className="main-root">
+                <Header style={{height:65}} className="header">
+                    <div className="logo" >中央平台</div>
+                    <div style={{float: 'right'}}>
+                        <Layout style={{"background": "white"}}>
+                            <Content>
+
+                            </Content>
+
+                            <Sider width={160} className="box"
+                                   style={{background: '#fff', height: '64px', justifyContent: 'flex-end'}}>
+                                <Button style={{"background": "#fff", "border": "none"}} icon="question-circle-o"
+                                        size="small" className="header-help">帮助</Button>
+
+                            </Sider>
+
+                        </Layout>
+                    </div>
+                </Header>
+                <Layout>
+                    <Router>
+                        <Switch>
+                            {/*外观.快速检索.历史查询*/}
+
+
+                            <Route component={NotFound}/>
+                        </Switch>
+                    </Router>
+
+                </Layout>
+
+
+            </Layout>
+        );
+    }
+}
+
--- app/index.js
+++ app/index.js
@@ -5,6 +5,9 @@ import {HashRouter as Router,Redirect, Switch, Route} from 'react-router-dom';
 // import {IndexStore} from './api.js';
 import NotFound from './notfound';
 
+import Centre from './centre/main/index';
+import Node from './node/main/index';
+
 // import Login from './login.js';
 // import App from './App.js';
 // import Main from './main';
@@ -14,9 +17,11 @@ import NotFound from './notfound';
 ReactDOM.render((
     <Router>
         <Switch>
-            <Redirect exact from='/' to='/signin'/>
+            <Redirect exact from='/' to='/centre'/>
             {/*<Route path="/signin" component={Login}/>*/}
-            {/*<Route path="/main" component={Main}/>*/}
+            <Route path="/centre" component={Centre}/>
+            <Route path="/node" component={Node}/>
+
             <Route component={NotFound}/>
         </Switch>
     </Router>
